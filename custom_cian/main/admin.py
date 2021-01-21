fe911045ac3fe3f46acb14d20fd15c4b3facc1d7
Katerina Frolova <ekaterina.frolova40@gmail.com>
2021-01-21 11:15:26 +0300

    Changed widget for flatpages

--- custom_cian/main/admin.py
+++ custom_cian/main/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.flatpages.models import FlatPage
+from django.contrib.flatpages.admin import FlatPageAdmin
+from django.db import models
 
-# Register your models here.
+from ckeditor.fields import RichTextField
+from ckeditor.widgets import CKEditorWidget
+
+
+class FlatPageAdmin(FlatPageAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': CKEditorWidget}
+    }
+
+
+admin.site.unregister(FlatPage)
+admin.site.register(FlatPage, FlatPageAdmin)
