7a78e6b6aaad6ff0662b679c78f5cd47b84946fa
Katerina Frolova <ekaterina.frolova40@gmail.com>
2021-01-22 17:03:38 +0300

    Added base models

--- custom_cian/main/models.py
+++ custom_cian/main/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Realty(models.Model):
+
+    name = models.CharField(verbose_name='Название', max_length=100)
+    price = models.IntegerField(verbose_name='Цена')
+    space = models.IntegerField(verbose_name='Площадь')
+    published_at = models.DateField(verbose_name='Дата публикации', auto_now_add=True)
+    description = models.TextField(verbose_name='Описание')
+    tags = models.ManyToManyField(Tag, related_name="Тэги")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    saller = models.ForeignKey(Saller, on_delete=CASCADE)
+    slug = models.CharField(verbose_name='Ссылка', max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Saller(models.Model):
+
+    first_name = models.CharField(verbose_name="Имя", max_length=50)
+    last_name = models.CharField(verbose_name="Фамилия", max_length=50)
+    registered_at = models.DateField(verbose_name="Дата регистрации", auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Category(models.Model):
+
+    name = models.CharField(verbose_name='Название', max_length=100)
+    slug = models.CharField(verbose_name='Ссылка', max_length=100)
+
+
+class Tag(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=100)
+    slug = models.CharField(verbose_name='Ссылка', max_length=100)
+    realty_list = models.ManyToManyField(Realty, related_name="Список недвижимости")
