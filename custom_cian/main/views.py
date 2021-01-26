d226de35aa535a0544b6a1a537edf0a77d4bf803
Katerina <ekaterina.frolova40@gmail.com>
2021-01-26 12:23:56 +0300

    Merged migrations and added user on index page

--- custom_cian/main/views.py
+++ custom_cian/main/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 def index(request):
     turn_on_block = True
-    params = {"turn_on_block": turn_on_block}
+    params = {"turn_on_block": turn_on_block, "current_user": request.user}
     return render(request, "main/index.html", params)
