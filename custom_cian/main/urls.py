b5b4e028987ea75fee32778aedd99a99b6d384de
Katerina Frolova <ekaterina.frolova40@gmail.com>
2021-01-28 12:38:57 +0300

    Added detail view and template

--- custom_cian/main/urls.py
+++ custom_cian/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('realty_list/', views.RealtyListView.as_view(), name="realty_list")
+    path('realty_list/', views.RealtyListView.as_view(), name="realty_list"),
+    path('realty_list/<slug:slug>/', views.RealtyDetailView.as_view(), name="realty_detail")
 ]
--- custom_cian/main/views.py
+++ custom_cian/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Realty
+from django.views.generic import DetailView, ListView
+from .models import Realty, Saller
 
 def index(request):
     turn_on_block = True
@@ -10,3 +10,14 @@ def index(request):
 
 class RealtyListView(ListView):
     model = Realty
+
+
+class RealtyDetailView(DetailView):
+    model = Realty
+
+    # def get_context_data(self, **kwargs):
+    #     # Call the base implementation first to get a context
+    #     context = super().get_context_data(**kwargs)
+    #     # Add in a QuerySet of all the books
+    #     context['book_list'] = Book.objects.all()
+    #     return context
