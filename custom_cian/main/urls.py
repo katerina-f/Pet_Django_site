80de50889de657d908b808ca03a23c4e7e3d6a0f
Katerina Frolova <ekaterina.frolova40@gmail.com>
2021-02-01 17:53:27 +0300

    Fixed urls

--- custom_cian/main/urls.py
+++ custom_cian/main/urls.py
@@ -10,6 +10,5 @@ urlpatterns = [
     path('realty_list/<slug:slug>/edit', views.RealtyUpdateView.as_view(), name="update_realty"),
     path('accounts/profile/<int:pk>/', views.SallerUpdateView.as_view(), name="update_profile"),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='main/login.html'), name="login"),
     path('realty/add/', views.RealtyCreateView.as_view(), name="add_realty")
 ]
