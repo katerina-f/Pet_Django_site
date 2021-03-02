3ef5d615cdf428427d31a8f41d15200cc647ba2f
Katerina Frolova <ekaterina.frolova40@gmail.com>
2021-03-02 14:29:51 +0300

    Fixing asgi settings for make websocket works

--- custom_cian/custom_cian/asgi.py
+++ custom_cian/custom_cian/asgi.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
+from channels.routing import get_default_application
+from django import setup
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'custom_cian.settings')
-
-application = get_asgi_application()
+setup()
+application = get_default_application()
